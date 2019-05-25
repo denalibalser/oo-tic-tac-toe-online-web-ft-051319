141401ec47e171d7ac948364592f1be91fcb11e1
Denali Balser <denali.balser167@gmail.com>
2019-05-25 13:20:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,6 +79,7 @@ class TicTacToe
         false
       end
       counter += 1
+      binding.pry
   end
   #def position_taken?(index_i)
     #((@board[index_i] == "X") || (@board[index_i] == "O"))
