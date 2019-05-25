bc2dc2a0207ac2d9d5dca41d4b3a8af93b939e92
Denali Balser <denali.balser167@gmail.com>
2019-05-25 13:00:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,6 +38,16 @@ class TicTacToe
   end
 
   def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    char = current_player
+    if valid_move?(index)
+      move(index, char)
+      display_board
+    else
+      turn
+    end
   end
 
   def turn_count
