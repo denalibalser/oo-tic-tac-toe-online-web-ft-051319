db3d5a51530ededd21762f0e42c629c2bb811817
Denali Balser <denali.balser167@gmail.com>
2019-05-25 13:05:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,5 +54,20 @@ class TicTacToe
     @board.count{|token| token == "X" || token == "O"}
   end
 
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+
+  def won?
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+      return win_combo
+    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+      return win_combo
+    end
+      false
+  end
+end
+
 
 end
